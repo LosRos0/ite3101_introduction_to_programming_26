ce50fa31ebd630c889a788ce56354709d3915a01
LosRos0 <washanwa71@gmail.com>
2026-09-06 17:58:22 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -4,4 +4,4 @@ exercises_complated = 13
 point_per_exercise = 5
 
 point_total = 100
-point_total += exercises_complated + point_per_exercise
+point_total += exercises_complated * point_per_exercise
