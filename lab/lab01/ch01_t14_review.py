3410444b9c4864770d6b6c2153eceee16c6295b8
LosRos0 <washanwa71@gmail.com>
2026-09-06 17:58:08 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -1,9 +1,7 @@
-skill_complated = "python syntax"
+skill_complated = "python Syntax"
 exercises_complated = 13
 
 point_per_exercise = 5
 
 point_total = 100
 point_total += exercises_complated + point_per_exercise
-
-print
