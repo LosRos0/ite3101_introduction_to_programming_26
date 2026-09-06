76b50b110e8407e0fc4057975c9a3b1675f07f0a
LosRos0 <washanwa71@gmail.com>
2026-09-06 18:00:57 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -1,9 +1,7 @@
 skill_complated = "python Syntax"
 exercises_complated = 13
 
-point_per_exercise = 5
+points_per_exercise = 5
 
 point_total = 100
-point_total += exercises_complated * point_per_exercise
-
-print("I got " + str(point_total) + "points!")
+point_total += exercises_complated * points_per_exercise
